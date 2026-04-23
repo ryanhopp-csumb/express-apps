614cadb0f0c1bf7ccd43415ac91a9a411598f758
Ryan Hopper <rhopper@csumb.edu>
2026-04-22 21:06:50 -0700

    updates to lab 7

--- lab7-db-admin/index.mjs
+++ lab7-db-admin/index.mjs
@@ -37,16 +37,66 @@ app.post('/addAuthor', async(req, res) => {
     let sql = `INSERT INTO authors
                 (firstName, lastName, dob, dod, biography)
                 VALUES
-                (?, ?, ?, ?, ?)`;
-    let sqlParams = [firstName, lastName, dob, sex, bio];
-
+                (?, ?, ?, ?, ?, ?)`;
+    let sqlParams = [firstName, lastName, dob, dod, sex, bio];
     const [newAuthors] = await pool.query(sql, sqlParams);
-
     res.redirect('/');
 });
 
-app.get('/addQuote', (req, res) => {
-   res.render('quote.ejs');
+app.get('/allAuthors', async(req, res) => {
+    let sql = `SELECT authorId, firstName, lastName
+                FROM authors
+                ORDER BY authorId`
+    const [authors] = await pool.query(sql);
+   res.render('allAuthors.ejs', {authors});
+});
+
+app.get('/updateAuthor', async(req, res) => {
+    let authorId = req.query.authorId;
+    let sql = `SELECT *, DATE_FORMAT(dob, '%Y-%m-%d') ISOdob, DATE_FORMAT(dod, '%Y-%m-%d') ISOdod
+                FROM authors
+                WHERE authorId = ?`
+    const [authorInfo] = await pool.query(sql, authorId);
+   res.render('updateAuthor.ejs', {authorInfo});
+});
+
+app.post('/updateAuthor', async(req, res) => {
+    let authorId = req.body.authorId;
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let dob = req.body.dob;
+    let dod = req.body.dod;
+    let sex = req.body.sex;
+    let sql = `UPDATE authors
+                SET
+                firstName = ?,
+                lastName = ?,
+                dob = ?
+                dod = ?
+                sex = ?
+                WHERE authorId = ?`
+    let sqlParams = [firstName, lastName, dob, dod, sex, authorId];
+    const [authorInfo] = await pool.query(sql, sqlParams);
+   res.redirect('/allAuthors.ejs', {authorInfo});
+});
+
+app.get('/deleteAuthor', async(req, res) => {
+    // let sql = `SELECT authorId, firstName, lastName
+    //             FROM authors
+    //             ORDER BY authorId`
+    // const [authors] = await pool.query(sql);
+   res.render('deleteAuthor.ejs');
+});
+
+
+
+app.get('/addQuote', async(req, res) => {
+    let sql = `SELECT authorId, firstName, lastName, category
+                FROM authors
+                NATURAL JOIN quotes
+                GROUP BY category`
+    const [authorOptions] = await pool.query(sql);
+   res.render('quote.ejs', {authorOptions});
 });
 
 app.post('/newQuote', async(req, res) => {
@@ -63,6 +113,47 @@ app.post('/newQuote', async(req, res) => {
     const [newQuotes] = await pool.query(sql, sqlParams);
 
     res.redirect('/');
+});
+
+app.get('/allQuotes', async(req, res) => {
+    let sql = `SELECT quote, quoteId
+                FROM quotes
+                ORDER BY quoteId`
+    const [allQuotes] = await pool.query(sql);
+   res.render('allQuotes.ejs', {allQuotes});
+});
+
+app.get('/updateQuote', async(req, res) => {
+    let quoteId = req.query.quoteId;
+    let sql = `SELECT *
+                FROM quotes
+                WHERE quoteId = ?`
+    const [quoteInfo] = await pool.query(sql, [quoteId]);
+
+    let sql1 = `SELECT authorId, firstName, lastName
+               FROM authors
+               ORDER BY lastName`;
+    const [authorList] = await pool.query(sql1);
+
+    let sql2 = `SELECT DISTINCT category
+               FROM quotes
+               ORDER BY category`;
+    const [categoryList] = await pool.query(sql2);
+   res.render('updateQuote.ejs', {quoteInfo, authorList, categoryList});
+});
+
+app.post('/updateQuote', async(req, res) => {
+    let quoteId = req.query.quoteId;
+    let sql = `SELECT *
+                FROM quotes
+                WHERE quoteId = ?`
+    const [quoteInfo] = await pool.query(sql, [quoteId]);
+
+    let sql1 = `SELECT authorId, firstName, lastName
+               FROM authors
+               ORDER BY lastName`;
+    const [authorList] = await pool.query(sql1);
+   res.render('updateQuote.ejs', {quoteInfo, authorList});
 });
 
 app.get("/dbTest", async(req, res) => {
